aa7ef9233799e6aa5cafdda6e6697cfdb4c00b3e
Mustafa Furkan BİLEN <furkanbilen4@gmail.com>
2022-11-24 22:25:38 +0300

    BST insert added

--- src/DataStructures/Tree/BinarySearchTree/BinarySearchTree.java
+++ src/DataStructures/Tree/BinarySearchTree/BinarySearchTree.java
@@ -1,11 +1,59 @@
 
 package DataStructures.Tree.BinarySearchTree;
 
+
 /**
  * 
  * @author Mustafa Furkan BİLEN <mustafafurkanbilen@gmail.com>
  * @since Nov 24, 2022 12:33:27 PM
  */
-public class BinarySearchTree {
 
+class Node{
+	
+	protected Integer key;
+	protected Node left;
+	protected Node right;
+	
+	protected Node(Integer key) {
+		this.key = key;
+		this.left=this.right=null;
+	}
+	
+	protected void displayData() {
+		System.out.print(key+" ");
+	}
+}
+public class BinarySearchTree{
+	
+	private Node root;
+	public void insert(Integer key) {
+		root = inserter(root,key);
+	}
+	
+	public Node inserter(Node node,Integer key) {
+		
+		if(root==null) {
+			root=new Node(key);
+			return root;
+		}
+		
+		if(key<node.key) 
+			root.left=inserter(root.left,key);
+		
+		else if(key>node.key)
+			root.right=inserter(root.right,key);
+			
+		
+		return root;
+	}
+	
+	public void display() {
+		
+		root.displayData();
+	}
+
+			
+		
+		
+	
 }
---
+++ src/DataStructures/Tree/BinarySearchTree/MyBST.java
@@ -0,0 +1,23 @@
+
+package DataStructures.Tree.BinarySearchTree;
+
+/**
+ * 
+ * @author Mustafa Furkan BİLEN <mustafafurkanbilen@gmail.com>
+ * @since Nov 24, 2022 9:40:50 PM
+ */
+public class MyBST {
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		BinarySearchTree s = new BinarySearchTree();
+		s.insert(6);
+		s.insert(5);
+		s.insert(4);
+		s.insert(7);
+		
+		s.display();
+	}
+
+}
